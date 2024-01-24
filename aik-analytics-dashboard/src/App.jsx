d4aef02e515a45abbbf14292ff8717a2db6da880
Aik-202 <ikogweassurance85@gmail.com>
2024-01-24 11:39:52 +0100

    Started development environment with git
    Installed tailwind and react-router-dom

---
+++ aik-analytics-dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import './App.css'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
