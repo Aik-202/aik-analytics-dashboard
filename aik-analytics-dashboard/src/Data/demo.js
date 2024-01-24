539d08495fbefc76d976a1f1ad8de4c4b376b420
Aik-202 <ikogweassurance85@gmail.com>
2024-01-24 23:33:36 +0100

    Added the required demo data

---
+++ aik-analytics-dashboard/src/Data/demo.js
@@ -0,0 +1,214 @@
+import { arrowRight, boxIcon, boxTick, coinIcon, cooper, corey, darkMode, dashboardIcon, discountIcon, infoIcon, jaydon, lightMode, logoutIcon, marcus, phillip, rotate, settingIcon, shoppingCart, trendUp, userIcon } from ".";
+
+export const lastOrder = [
+    {
+        id: 1,
+        img: marcus,
+        name: 'Marcus Bergson',
+        date: 'Nov 15, 2023',
+        amount: 80000,
+        status: 'Paid',
+    },
+    {
+        id: 2,
+        img: jaydon,
+        name: 'Jaydon Vaccaro',
+        date: 'Nov 15, 2023',
+        amount: 150000,
+        status: 'Refund',
+    },
+    {
+        id: 3,
+        img: corey,
+        name: 'Corey Schleifer',
+        date: 'Nov 14, 2023',
+        amount: 87000,
+        status: 'Paid',
+    },
+    {
+        id: 4,
+        img: cooper,
+        name: 'Cooper Press',
+        date: 'Nov 14, 2023',
+        amount: 100000,
+        status: 'Refund',
+    },
+    {
+        id: 5,
+        img:  phillip,
+        name: 'Phillip Lubin',
+        date: 'Nov 13, 2023',
+        amount: 78000,
+        status: 'Paid',
+    },
+]
+
+export const summary = [
+    {
+        id: 1,
+        img: boxTick,
+        title: 'Total Order',
+        state: 'profit',
+        total: '350',
+    },
+    {
+        id: 2,
+        img: rotate,
+        title: 'Total Refund',
+        state: 'loss',
+        total: '270',
+    },
+    {
+        id: 3,
+        img: shoppingCart,
+        title: 'Average Sales',
+        state: 'loss',
+        total: '1567',
+    },
+    {
+        id: 4,
+        img: coinIcon,
+        title: 'Total Income',
+        state: 'profit',
+        total:'$350.000',
+    },
+]
+
+export const topPlateform = [
+    {
+        id: 1,
+        name: 'Book Bazaar',
+        percent: '+15%',
+        color: '#6160DC',
+        value: 50,
+        amount: 2500000,
+    },
+    {
+        id: 2,
+        name: 'Artisan Aisle',
+        percent: '+10%',
+        color: '#54C5EB',
+        value: 40,
+        amount: 1800000,
+    },
+    {
+        id: 3,
+        name: 'Toy Troop',
+        percent: '+8%',
+        color: '#FFB74A',
+        value: 30,
+        amount: 1200000,
+    },
+    {
+        id: 4,
+        name: 'XStore',
+        percent: '+5%',
+        color: '#FF4A55',
+        value: 20,
+        amount: 500000,
+    },
+]
+const chartdata = [
+    {
+        name: 'Jan',
+        "Sales Value": 6.000,
+    },
+    {
+        name: 'Feb',
+        "Sales Value": 19.000,
+    },
+    {
+        name: 'Mar',
+        "Sales Value": 4.000,
+    },
+    {
+        name: 'Apr',
+        "Sales Value": 29.000,
+    },
+    {
+        name: 'Mei',
+        "Sales Value": 9.000,
+    },
+    {
+        name: 'Jun',
+        "Sales Value": 45.000,
+    },
+    {
+        name: 'Jul',
+        "Sales Value": 9.000,
+    },
+    {
+        name: 'Aug',
+        "Sales Value": 20.000,
+    },
+    {
+        name: 'Sep',
+        "Sales Value": 34.000,
+    },
+    {
+        name: 'Okt',
+        "Sales Value": 4.000,
+    },
+    {
+        name: 'Nov',
+        "Sales Value": 30.000,
+    },
+    {
+        name: 'Des',
+        "Sales Value": 26.000,
+    },
+]
+
+export const dashboardList =[
+    {
+        id: 1,
+        img1: dashboardIcon,
+        title: 'Overview',
+    },
+    {
+        id: 2,
+        img1: trendUp,
+        title: 'Trends',
+    },
+    {
+        id: 3,
+        img1: userIcon,
+        title: 'Account',
+    },
+    {
+        id: 4,
+        img1: boxIcon,
+        title: 'Orders',
+    },
+    {
+        id: 5,
+        img1: discountIcon,
+        title: 'Discount',
+    },
+    {
+        id: 6,
+        img1: infoIcon,
+        title: 'About',
+    },
+    {
+        id: 7,
+        img1: lightMode,
+        img2: darkMode,
+        title: 'Theme',
+    },
+    {
+        id: 8,
+        img1: arrowRight,
+        title: 'Home',
+    },
+    {
+        id: 9,
+        img1: settingIcon,
+        title: 'Settings',
+    },
+    {
+        id: 10,
+        img1: logoutIcon,
+        title: 'Log out',
+    },
+]
